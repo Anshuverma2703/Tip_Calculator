01367d340401aa3d038587ff331e67fc40587d35
Anshu Verma <Anshuverma2703@gmail.com>
2022-10-25 23:25:20 +0530

    Update Index.js
    some changes in totalperperson

--- Index.js
+++ Index.js
@@ -71,8 +71,8 @@ else{
 
 function calculateTip() {
   if (peopleValue >= 1) {
-    let tipAmount = (billValue * tipvalue) / peopleValue;
-    let total = (billValue * tipAmount) / peopleValue;
+    let tipAmount = (billValue + tipvalue) / peopleValue;
+    let total = (billValue + tipAmount) / peopleValue;
     tipPerPerson.innerHTML = "₹" + tipAmount.toFixed(2);
     totalPerPerson.innerHTML = "₹" + total.toFixed(2);
   }
